76cfd696e649c470895d1bacf24414710cd69815
Thomas Sluyter <thomas.sluyter@unixerius.nl>
2016-04-04 18:57:20 +0000

    Fix wrong version of sysreplace.pp
    Can you tell that I'm getting used to branching and adding files? Turns out that I forgot to do another "git add" after making a few edits to sysreplace.pp. Which rendered it unusable. I'm learning though!

--- manifests/sysreplace.pp
+++ manifests/sysreplace.pp
@@ -2,24 +2,24 @@
 
 class boks::sysreplace
 (
-$sysreplace_set_to = 'on',
+$sysreplace_set_to = "on",
 )
 
 inherits boks
 
 {
 
-  if ${sysreplace_set_to} == "on" {
+  if $sysreplace_set_to == "on" {
     exec { "sysreplace_on":
-      command => '${boks_lib}/sysreplace replace',
-      unless  => 'grep -i "SSM_ACTIVE=true" $env_file',
+      command => "${boks_lib}/sysreplace replace",
+      unless  => "/bin/grep -i \"SSM_ACTIVE=true\" $env_file",
       require => Package["${package_name}"],
     }
   }
   else {
     exec { "sysreplace_off":
-      command => '${boks_lib}/sysreplace restore',
-      onlyif  => 'grep -i "SSM_ACTIVE=true" $env_file',
+      command => "${boks_lib}/sysreplace restore",
+      onlyif  => "/bin/grep -i \"SSM_ACTIVE=true\" $env_file",
       require => Package["${package_name}"],
     }
   }
