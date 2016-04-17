cf8d691c0a9fd8ba84d808c25125874481e5e405
Thomas Sluyter <thomas.sluyter@unixerius.nl>
2016-04-17 16:28:55 +0000

    Missed variable safety with double quotes in rootaccount.pp. Boy do I look stupid.

--- manifests/rootaccount.pp
+++ manifests/rootaccount.pp
@@ -13,7 +13,7 @@ inherits boks {
   $rootpasshash = hiera('boks::rootpasshash')
 
   @@exec { "Root account of ${::hostname}":
-    command => "${boks_sbin}/mkbks -l ${::fqdn}:root -u 0 -g 0 -p ${rootpasshash} -h ${roothome} -r \"Superuser managed by Puppet\" -s ${rootshell} -P ${rootuserclass}",
+    command => "${boks_sbin}/mkbks -l ${::fqdn}:root -u 0 -g 0 -p \"${rootpasshash}\" -h \"${roothome}\" -r \"Superuser managed by Puppet\" -s \"${rootshell}\" -P ${rootuserclass}",
     unless  => "${boks_sbin}/lsbks -l ${::fqdn}:root | /bin/grep \":root\"",
     tag     => "rootaccount",
   }
